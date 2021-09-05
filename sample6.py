6402232834068c0fd776ae740cc31beede90dda8
mercy34mercy <etukobamasatyan@gmail.com>
2021-09-05 17:27:00 +0900

    yahooでやることにした

--- sample6.py
+++ sample6.py
@@ -5,13 +5,14 @@ import random
 import re
 
 def image(data):
-    Res = requests.get("https://www.google.com/search?hl=jp&q=" + data + "&btnG=Google+Search&tbs=0&safe=off&tbm=isch")
+    Res = requests.get("https://search.yahoo.co.jp/search?p=%E3%81%82%E3%81%84%E3%81%86%E3%81%88%E3%81%8A&fr=top_ga1_sa&ei=UTF-8&ts=4438&aq=-1&oq=&at=&ai=db0882f1-d8e1-4898-9732-8d47ea5cacf5")
     Html = Res.text
     Soup = BeautifulSoup(Html,'lxml')
 
-    #kanrens = Soup.find_all('span')
+    kanren = Soup.find_all('a')
     # kanrens = Soup.find_all()
-    # kanren = Soup.find_all("span","hIOe2")
+    #kanren = Soup.select('Unit__listItem')
+    #, class_="Unit__listItem"
     # #a = kanrens[2].contents[0]
     # kanren = random.choice(kanrens).get("aria-label")
 
@@ -27,14 +28,18 @@ def image(data):
         # ad_basics = ad['aria-label']
         #print(ad)
 
-    ad = Soup.select(".PKhmud")
-    print(ad)
-    print(Html)
+    # ad = Soup.select(".PKhmud")
+    print(kanren)
+    #print(Html)
+    for elem in kanren:
+        print(elem)
 
 
             
 
  
+
+#<li class="Unit__listItem">
 
 
 
