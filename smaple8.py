1694f42b6df56919f5f017f09e7c661f68ad65e8
mercy34mercy <masakun@DESKTOP-PLC0L1Q.localdomain>
2021-09-05 21:40:40 +0900

    ginzaつかえるようになった

--- smaple8.py
+++ smaple8.py
@@ -1,4 +1,10 @@
 
 import spacy
-nlp = spacy.load('ja_ginza')
+nlp = spacy.load('ja_ginza_electra')
+doc = nlp('spaCy はオープンソースの自然言語処理ライブラリです。学習済みの統計モデルと単語ベクトルが付属しています。')
 
+for sent in doc.sents:
+    for token in sent:
+        print(token.i, token.orth_, token.lemma_, token.pos_, token.tag_, token.dep_, token.head.i)
+    print('EOS')
+#/home/masakun/.local/lib/python3.8/site-packages/spacy/cli/debug_data.py
